7ae5c3df8fbdecdf9c02c47199283e228c79f0b3
lee2752 <69233948+leedh2752@users.noreply.github.com>
2023-06-26 16:05:55 +0900

    [fix]: packagr을 위한 파일 추가

---
+++ public-api.ts
@@ -0,0 +1,14 @@
+/*
+ * Public API Surface of serengeti-chat
+ */
+
+export * from './src/app/app.module';
+export * from './src/app/app.component';
+
+export * from './src/app/metering/list/list.component';
+export * from './src/app/metering/detail/detail.component';
+export * from './src/app/metering/detail/detail-log/detail-log.component';
+export * from './src/app/metering/detail/detail-table/detail-table.component';
+export * from './src/app/statistics/statistics.component';
+export * from './src/app/statistics/statistics-detail-table/statistics-detail-table.component';
+export * from './src/app/statistics/chart-table/chart-table.component';
