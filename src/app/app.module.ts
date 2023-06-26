ca14de1aa88696831c73e9546ac437bad54563f1
lee2752 <69233948+leedh2752@users.noreply.github.com>
2023-06-26 16:05:08 +0900

    [fix]: app module export component 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -86,6 +86,15 @@ export function createTranslateLoader(http: HttpClient) {
     Configurations,
     ThemeService
   ],
+  exports: [
+    MeteringListComponent,
+    MeteringDetailListComponent,
+    StatisticsComponent,
+    MeteringDetailLogComponent,
+    MeteringDetailTableComponent,
+    StatisticsDetailTableComponent,
+    StatisticsChartTableComponent,
+  ],
   entryComponents: [
     StatisticsDetailTableComponent
   ],
