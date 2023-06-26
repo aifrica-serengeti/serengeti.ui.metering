72cc98add16aefe99d9cddb0111b2c249eba9fa9
lee2752 <69233948+leedh2752@users.noreply.github.com>
2023-06-26 16:05:41 +0900

    [fix]: setLogEntity function추가

--- src/app/metering/detail/detail-table/MeteringLog.ts
+++ src/app/metering/detail/detail-table/MeteringLog.ts
@@ -2,13 +2,17 @@ export class MeteringLog {
   constructor(metering: any) {
     this.meteringName = metering.meteringName;
     this.meteringType = metering.meteringType;
-    this.id = metering.meteringLogEntityList[0].id;
-    this.state = metering.meteringLogEntityList[0].state;
-    this.cpu = metering.meteringLogEntityList[0].cpu;
-    this.gpu = metering.meteringLogEntityList[0].gpu;
-    this.memory = metering.meteringLogEntityList[0].memory;
-    this.disk = metering.meteringLogEntityList[0].disk;
-    this.updateDate = metering.meteringLogEntityList[0].updateDate;
+
+  }
+
+  public setLogEntity(logEntity: {id, state, cpu, gpu, memory, disk, updateDate}) {
+    this.id = logEntity.id;
+    this.state = logEntity.state;
+    this.cpu = logEntity.cpu;
+    this.gpu = logEntity.gpu;
+    this.memory = logEntity.memory;
+    this.disk = logEntity.disk;
+    this.updateDate = logEntity.updateDate;
   }
 
   id;
